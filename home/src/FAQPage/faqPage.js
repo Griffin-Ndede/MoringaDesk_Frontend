bf82b29e0eb57f05fb666c5f879b969e6644b369
wayne-otido <otidowayne@gmail.com>
2023-10-27 22:55:51 +0300

    Created question page

--- home/src/FAQPage/faqPage.js
+++ home/src/FAQPage/faqPage.js
@@ -19,8 +19,8 @@ function FaqPage(){
                     <FaqCard title={"How to display a list as a string in Python?"} />
                 </div>
                 <div id="recents">
-                    <h2 id="Recents">Recents</h2>
-                    <RecentCard username={"Joanne"} title={"How to display two elements side by side in React?"} tags={["ReactJS", "JavaScript"]} replyCount={2} date={"26/10/23"} />
+                    <h2 id="Recents">Recent Questions</h2>
+                    <RecentCard username={"Joanne"} title={"How to display two elements side by side in React?"} tags={["ReactJS", "CSS"]} replyCount={2} date={"26/10/23"} />
                     <RecentCard username={"Lewis"} title={"How do I loop through a string in Python?"} tags={["Python"]} replyCount={7} date={"25/10/23"} />
                     <RecentCard username={"Peter"} title={"How to split a string at a specified character in JavaScript"} tags={["JavaScript"]} replyCount={3} date={"23/10/23"} />
                     <RecentCard username={"Griffin"} title={"How can I pick the last letter of every word in an List"} tags={["Python"]} replyCount={1} date={"18/10/23"} />
