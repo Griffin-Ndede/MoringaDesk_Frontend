87b54f17b9445c2341b61d0540f8b3379d04173c
peterkomo <mwangipeterkomo@gmail.com>
2023-10-30 08:45:45 +0300

    added needed images

--- home/src/homePage/homepage.js
+++ home/src/homePage/homepage.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./homepage.css";
+import img1 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/EML.png"
+import img2 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/faq.png"
+import img3 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/Filter.png"
+import img4 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/IG.jpeg"
+import img5 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/image.png"
+import img6 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/MAP.jpeg"
+import img7 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/NOTIFICATON.png"
+import img8 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/phone.png"
+import img9 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/posting.png"
+import img10 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/questions.jpeg"
+import img11 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/search.jpg"
+import img12 from "/home/mwangi/MoringaDesk_Frontend/home/src/homePage/images/TWEETER.png"
