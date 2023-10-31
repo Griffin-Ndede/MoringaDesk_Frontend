fb16febd61b29722c15a3331750dadb0d7233315
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-10-31 23:20:00 +0300

    Updated tags header styling

--- home/src/tagsPage/tagsPage.js
+++ home/src/tagsPage/tagsPage.js
@@ -8,10 +8,10 @@ function TagsPage() {
     <div>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
     </div>
-    </div>
-      <div id='searchbar'>
+    <div id='searchbar'>
         <input type="text" id="searchInput" placeholder="Filter by tag name"></input>
-      </div>
+    </div>
+    </div>
       <div className='card-container'>
         <div className='card'>
           <h3>Phase 0</h3>
