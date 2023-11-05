7ad96a34dbbf2cc43debeef3e9e5d2e9ffa51bc7
wayne-otido <otidowayne@gmail.com>
2023-11-05 14:08:29 +0300

    implemented get requests for question page

--- home/src/App.js
+++ home/src/App.js
@@ -9,9 +9,22 @@ import UserPage from './userPage/userPage';
 import LoginPage from './login/login';
 import CreateAccount from './signup/sighnup';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 
 
 function App() {
+  const [ questions, setQuestions ] = useState([])
+  const initialState = { questionId: 1 }
+
+  useEffect(()=>{
+    fetch('/questions')
+    .then((res)=> res.json())
+    .then(data => {
+      setQuestions(data)
+    })
+  }, [])
+
   return (
     <>
       <NavBar />
@@ -19,10 +32,10 @@ function App() {
           <Route exact path="/" element={<CreateAccount />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route path='/home' element={<HomePage />} />
-          <Route path='/FAQs' element={<FaqPage />}/>
+          <Route path='/FAQs' element={<FaqPage questions={questions} />}/>
           <Route path='/tags' element={<TagsPage />} />
           <Route path='/user' element={<UserPage />} />
-          <Route path='/questions' element={<QuestionPage />} />
+          <Route path='/questions/:id' element={<QuestionPage />} />
       </Routes>
     </>
   );
--- home/src/questionPage/questionPage.js
+++ home/src/questionPage/questionPage.js
@@ -1,18 +1,34 @@
 import './questions.css'
 import ResponseCard from './responseCard';
 import PostResp from '../popUps/postResponse';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { CodeBlock } from "react-code-blocks";
+import { useSelector } from "react-redux";
 
 function QuestionPage(){
+
+    const qnId = useSelector((state) => state.theStore.value);
+
+    useEffect(()=>{
+        fetch(`/questions/${qnId}`)
+        .then((res)=> res.json())
+        .then(data => {
+          setQuestion(data)
+        })
+      }, [])
+      
     const [ resp, setResp ] = useState(false)
+    const [ question, setQuestion ] = useState([])
+    
 
     function addResp(){
         setResp(!resp)
     }
+
     return(
         <>
             <div id="questionPage">
+                
                 <div className="questionDiv">
                     <div className="question">
                         <img className='actionDropDown' alt='option menu' src='https://static.thenounproject.com/png/892510-200.png'/>
@@ -21,21 +37,21 @@ function QuestionPage(){
                             <button className='deleteButtons'>Delete</button>
                         </div>
                         <img className="userIcon" alt="user icon" src="https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur-gris.png" />
-                        <h3 className="questionUserName">@George:</h3>
-                        <h1 className="questionTitle">How to convert an array to a string in JavaScript?</h1>
-                        <p className='questionDescription'>I am trying to take an array of N strings and convert it into one long string. How can i do this?</p>
+                        <h3 className="questionUserName">@{question.user?.username}:</h3>
+                        <h1 className="questionTitle">{question.title}</h1>
+                        <p className='questionDescription'>{question.description}</p>
                         <div className='questionCodeBlock'>
                             <CodeBlock
-                                text={'let Array = ["My", "name", "is", "Sam"]; \nconsole.log(Array);'}
-                                language='javascript'
+                                text={question.code}
                                 showLineNumbers={false}
                             />
                         </div>
                     </div>
                 </div>
                 <h2 id="Solutions">Solutions: </h2>
-                <ResponseCard user={"Wayne"} solution={"Use the .join array method with a space as a seperator."} code={'let Array = ["My", "name", "is", "Sam"]; \nlet Sentence = Array.join(" ");'} votes={4}/>
-                <ResponseCard user={"Otido"} solution={"Use the .join array method with a space as a seperator."} code={'let Array = ["My", "name", "is", "Sam"]; \nlet Sentence = Array.join(" ");'} votes={1}/>
+                {question.responses?.map((response)=>(
+                        <ResponseCard user={response.user.username} solution={response.suggestion} code={response.code} votes={response.votes} />
+                ))}
                 <button className="addButtons1" onClick={addResp}> + </button>
                 {resp ? <div className="popUpBackground"><button className='closePopUp' onClick={addResp} >Close</button><PostResp /></div>: <></>}
 
