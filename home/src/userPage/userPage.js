92dfaa22c373813a27338691ba2b83e9c32c9988
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-11-08 16:51:05 +0300

    Changed user page

--- home/src/userPage/userPage.js
+++ home/src/userPage/userPage.js
@@ -1,116 +1,104 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import './userPage.css'
 
-function UserPage(){
-    const [saves, setSaves] = useState([]);
-    const [responses, setResponses] = useState([]);
-    const [questions, setQuestions] = useState([]);
-    // const userId = 7;
-    const userId = localStorage.getItem('userId');
+function UserPage() {
+  const [saves, setSaves] = useState([]);
+  const [responses, setResponses] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [activeButton, setActiveButton] = useState('Saves');
 
-    useEffect(() => {
-        if (userId) {
-          fetch(`https://moringa-yjml.onrender.com/saves/${userId}`)
-            .then((res) => res.json())
-            .then((saves) => setSaves(saves));
-        }
-      }, [userId]);
+  const userId = localStorage.getItem('userId');
+  const userName = localStorage.getItem('userName');
 
-    useEffect(() => {
-        if (userId) {
-            fetch(`https://moringa-yjml.onrender.com/responses/${userId}`)
-                .then((res) => res.json())
-                .then((responses) => setResponses(responses));
-        }
-    }, [userId]);
+  useEffect(() => {
+    if (userId) {
+      fetch(`https://moringa-yjml.onrender.com/saves/${userId}`)
+        .then((res) => res.json())
+        .then((saves) => setSaves(saves));
+    }
+  }, [userId]);
 
-    useEffect(() => {
-        if (userId) {
-            fetch(`https://moringa-yjml.onrender.com/questions/${userId}`)
-                .then((res) => res.json())
-                .then((questions) => setQuestions(questions));
-        }
-    }, [userId]);
+  useEffect(() => {
+    if (userId) {
+      fetch(`https://moringa-yjml.onrender.com/responses/${userId}`)
+        .then((res) => res.json())
+        .then((responses) => setResponses(responses));
+    }
+  }, [userId]);
 
-    return(
+  useEffect(() => {
+    if (userId) {
+      fetch(`https://moringa-yjml.onrender.com/questions/${userId}`)
+        .then((res) => res.json())
+        .then((questions) => setQuestions(questions));
+    }
+  }, [userId]);
+
+  const handleClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+
+  return (
+    <>
+      <div id='header'>
+        <img src='./Default_pfp.png' alt='Default pfp' />
+        <div id="header-content">
+          <div>
+            <h1>{userName}</h1>
+            {/* <p>Member since 2022</p> */}
+          </div>
+          <div>
+          <button onClick={() => handleClick('Saves')}>Saves</button>
+            <button onClick={() => handleClick('My Questions')}>Questions</button>
+            <button onClick={() => handleClick('My Responses')}>Responses</button>
+          </div>
+        </div>
+      </div>
+
+      {activeButton === 'Saves' && (
         <>
-            <div id='header'>
-    <img src='./Default_pfp.png' alt='Default pfp' />
-    <div id="header-content">
-        <div>
-            <h1>Wayne Otido</h1>
-            <p>Member since 2022</p>
-        </div>
-        <div id="header-buttons">
-            <button>Saves</button>
-            <button>My Questions</button>
-            <button>My Responses</button>
-        </div>
-    </div>
-</div>
-            {/* <h2>My Questions</h2>
-            <div className='questions'>
-                <div>
-                    <h3>Problem title</h3>
-                    <button>tag 1</button>
-                </div>
-                <div>
-                    <h3>Problem title</h3>
-                    <button>tag 1</button>
-                    <button>tag 2</button>
-                </div>
-                <div>
-                    <h3>Problem title</h3>
-                    <button>tag 1</button>
-                    <button>tag 2</button>
-                </div>
-            </div> */}
-            {/* <h2>My Responses</h2>
-            <div className='questions'>
-                <div>
-                    <h3>Problem title</h3>
-                    <button>tag 1</button>
-                </div>
-                <div>
-                    <h3>Problem title</h3>
-                    <button>tag 1</button>
-                    <button>tag 2</button>
-                </div>
-                <div>
-                   <h3>Problem title</h3>
-                   <button>tag 1</button>
-                   <button>tag 2</button>
-                </div> */}
-            {/* </div> */}
-            <h2>My Questions</h2>
-            <div className='questions'>
-                {questions.map((question) => (
-                    <div key={question.id}>
-                        <h3>{question.title}</h3>
-                        <p>{question.description}</p>
-                    </div>
-                ))}
-            </div>
-            <h2>My Responses</h2>
-            <div className='questions'>
-                {responses.map((response) => (
-                    <div key={response.id}>
-                        <h3>{response.suggestion}</h3>
-                        <p>{response.code}</p>
-                    </div>
-                ))}
-            </div>
-                <h2>My Saves</h2>
-                <div className="questions">
-                    {saves && saves.map((save) => (
-                    <div key={save.id}>
+          <h2>My Saves</h2>
+          <div className="questions">
+            {saves.map((save) => (
+              <div key={save.id}>
                 <h3>{save.question.title}</h3>
                 <button>{save.question.tag}</button>
-            </div>
-          ))}
-        </div>
+              </div>
+            ))}
+          </div>
         </>
-    )
+      )}
+
+      {activeButton === 'My Questions' && (
+        <>
+          <h2>My Questions</h2>
+          <div className="questions">
+            {questions.map((question) => (
+              <div key={question.id}>
+                <h3>{question.title}</h3>
+                <p>{question.description}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+
+      {activeButton === 'My Responses' && (
+        <>
+          <h2>My Responses</h2>
+          <div className="questions">
+            {responses.map((response) => (
+              <div key={response.id}>
+                <h3>{response.suggestion}</h3>
+                <p>{response.code}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
-export default UserPage
+export default UserPage;
